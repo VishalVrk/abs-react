1220ae3aaeff3193f77d085cb1f0b102e6fc904e
unknown <vishal.rk@royalcyber.com>
2020-04-15 18:37:12 +0530

    changed width to auto

--- src/components/utlis/Facebook.js
+++ src/components/utlis/Facebook.js
@@ -14,7 +14,7 @@ class Facebook extends Component {
                     fontWeight:"bold"
                 }}>Facebook</h2>
                     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIndia-vs-England-600561000134334%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2423540887875789"
-             width="400" height="400" style={{border:"none",
+             width="auto" height="400" style={{border:"none",
                 overflow:"hidden", position:"relative"}} scrolling="no" frameborder="0" 
              allowTransparency="true" allow="encrypted-media"></iframe>
             </div>
