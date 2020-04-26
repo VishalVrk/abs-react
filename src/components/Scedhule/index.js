000c497324df8246f441e909b14c0775d2f932a4
unknown <vishal.rk@royalcyber.com>
2020-04-26 15:28:05 +0530

    Added text in gallery page

--- src/components/Scedhule/index.js
+++ src/components/Scedhule/index.js
@@ -12,7 +12,7 @@ export default class Scedhule extends Component {
     heading:"Scedhule an Meeting"
   }
 
-  sendEmail = _ => {
+  sendEmail = e => {
     const { email } = this.state;
     fetch(`https://abs-mailer.herokuapp.com/send-email?name=${email.name}&sender=${email.sender}&topic=${email.subject}&text=${email.text}`) //query string url
       .catch(err => console.error(err));
@@ -35,14 +35,16 @@ export default class Scedhule extends Component {
        <div className="contact-form">
        <div class="wrap-input1 validate-input" data-validate = "Name is required">
         <input class="input1" type="text" name="name" placeholder="Name"
-        value={email.name} 
-        onChange={e => this.setState({ email: { ...email, name: e.target.value } })}
+        value={email.name} required
+        onChange={e => 
+          
+          this.setState({ email: { ...email, name: e.target.value } })}
         />
         <span class="shadow-input1"></span>
      </div>
      <div class="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-        <input class="input1" type="text" name="email" placeholder="Email"
-         value={email.sender}
+        <input class="input1" type="email" name="email" placeholder="Email"
+         value={email.sender} required
          onChange={e =>
           this.setState({ email: { ...email, sender: e.target.value }})}
         />
@@ -50,13 +52,13 @@ export default class Scedhule extends Component {
      </div>
      <div class="wrap-input1 validate-input" data-validate = "Subject is required">
         <input class="input1" type="text" name="subject" placeholder="Subject"
-        value={email.subject}
+        value={email.subject} required
         onChange={e => this.setState({ email: { ...email, subject: e.target.value } })}
         />
         <span class="shadow-input1"></span>
      </div>
      <div class="wrap-input1 validate-input" data-validate = "Message is required">
-        <textarea class="input1" name="message" placeholder="Message"
+        <textarea class="input1" name="message" placeholder="Message" required
         value={email.text}  onChange={e => this.setState({ email: { ...email, text: e.target.value } })}
         />
         <span class="shadow-input1"></span>
